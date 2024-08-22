2795705228a0ddb7e052491a4f927f7d6110ea48
Tomas Zorzano <40808662+tomaszorzano@users.noreply.github.com>
2024-08-22 16:35:50 -0300

    Add files via upload

--- informacion-instancias/Agente/agente.py
+++ informacion-instancias/Agente/agente.py
@@ -9,14 +9,11 @@ def obtener_usuarios():
     sistema_operativo = platform.system()
     
     if sistema_operativo == "Windows":
-        # Obtener usuarios en Windows
         resultado = subprocess.run(['net', 'user'], capture_output=True, text=True)
         usuarios = resultado.stdout.splitlines()
-        # Filtrar la salida para obtener solo los nombres de usuario
         usuarios = [line.strip() for line in usuarios if line.strip() and not line.startswith("-------------------------------------------------------------------------------")]
     
     elif sistema_operativo == "Linux":
-        # Obtener usuarios en Linux
         with open('/etc/passwd') as f:
             for line in f:
                 partes = line.split(':')
@@ -32,8 +29,8 @@ def obtener_informacion():
             for proc in psutil.process_iter(['pid', 'name', 'status']) 
             if proc.info['status'] == psutil.STATUS_RUNNING
         ],
-        "usuarios_activos": [user._asdict() for user in psutil.users()],  # Convertir a dict
-        "todos_los_usuarios": obtener_usuarios(),  # Todos los usuarios en la máquina
+        "usuarios_activos": [user._asdict() for user in psutil.users()], 
+        "todos_los_usuarios": obtener_usuarios(),  
         "sistema_operativo": platform.system(),
         "version_sistema": platform.version()
     }
@@ -46,10 +43,6 @@ def enviar_informacion(info, url):
 if __name__ == "__main__":
     base_url = "https://t3jxqxig0f.execute-api.us-east-1.amazonaws.com/dev"
     endpoint_guardar = f"{base_url}/guardar"
-    
-    # Obtener información de la instancia
     info = obtener_informacion()
-    
-    # Enviar información a la API
     status = enviar_informacion(info, endpoint_guardar)
-    print(f"Envío completado con estado: {status}")
+    print(f"estado: {status}")
